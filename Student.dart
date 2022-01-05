287a5a35aed77b891290871aad22c264648628fd
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-05 23:59:46 +0700

    Day 3

---
+++ Student.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+class info {
+  int? id;
+  String? name;
+  double? total;
+}
+
+void main() {
+  int i;
+  stdout.write("Input number of student to add: ");
+  int? n = int.parse(stdin.readLineSync()!);
+  for (int i = 1; i <= n; i++) {
+    stdout.write("Student ID: ");
+    int? id = int.parse(stdin.readLineSync()!);
+    stdout.write("Student name: ");
+    String? name = stdin.readLineSync();
+    stdout.write("Score of Khmer: ");
+    double? kh = double.parse(stdin.readLineSync()!);
+    stdout.write("Score of Math: ");
+    double? m = double.parse(stdin.readLineSync()!);
+    stdout.write("Score of English: ");
+    double? en = double.parse(stdin.readLineSync()!);
+    double total = (kh + m + en) / 3;
+  }
+}
