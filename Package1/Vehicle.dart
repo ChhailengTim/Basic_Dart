9674fc2427b72fe1ef6594d843be653abc079227
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-18 23:58:19 +0700

    Day 10

---
+++ Package1/Vehicle.dart
@@ -0,0 +1,11 @@
+class Vehicle{
+  String? brand;
+  String? type;
+  String? name;
+  Vehicle(String brand,String type,String name)
+  {
+    this.brand=brand;
+    this.type=type;
+    this.name=name;
+  }
+}
