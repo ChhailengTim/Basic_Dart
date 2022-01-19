8af4866b3a06beca0a6f6533ce87de95938af538
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-20 00:09:33 +0700

    Mainresult is for print all data

---
+++ Package2/MainResult.dart
@@ -0,0 +1,19 @@
+import 'ITStudent.dart';
+import 'MathStudent.dart';
+import 'Person.dart';
+import 'Student.dart';
+
+void main(){
+  //Person p=new Person("Kim", "Female", 18);
+  //p.Display();
+  //print("==================");
+  //Student s=new Student("Dara", "Male", 20, "Computer Science", 4.5, "M3");
+  //s.Display();
+  //print("==================");
+  //ITStudent I=new ITStudent("Chhaileng", "Male", 23, "Information Technology", 5.5, "E7", "Chenda Sovisal", 101, "Night");
+  //I.Display();
+  //print("==================");
+  MathStudent m=new MathStudent("Roth", "Female", 21, "Mathematics", 4.3, "B", "Sok Somnang", 105, "Morning");
+  m.Display();
+  print("==================");
+}
