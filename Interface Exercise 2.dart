aa96f87ec7e2f54f248ba2f27a5872a8417078e9
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-01-22 10:36:27 +0700

    Update Interface Exercise 2.dart

--- Interface Exercise 2.dart
+++ Interface Exercise 2.dart
@@ -33,7 +33,28 @@ class MovablePoint implements IMovable{
     print("-xSpeed & ySpeed");
   }
 }
-
+class MovableCircle extends MovablePoint implements IMovable{
+  int? radius;
+  MovablePoint? center;
+  MovableCircle(int x,int y,int xSpeed,int ySpeed,int radius):super(x,y,xSpeed,ySpeed){
+    this.radius=radius;
+  }
+  String toString(){
+    return ("MovableCircle");
+  }
+  void moveUp(){
+    print("x & -y");
+  }
+  void moveDown(){
+    print("-x & y");
+  }
+  void moveLeft(){
+    print("xSpeed & -ySpeed");
+  }
+  void moveRight(){
+    print("-xSpeed & ySpeed");
+  }
+}
 void main(){
   MovablePoint m=new MovablePoint(1, 2, 3, 3);
   print(m.toString());
@@ -45,6 +66,16 @@ void main(){
   m.moveLeft();
   print("${m.ySpeed}");
   m.moveRight();
+  MovableCircle mc=new MovableCircle(12, 14, 45, 24, 42);
+  print(mc.toString());
+  print("${mc.x}");
+  mc.moveUp();
+  print("${mc.xSpeed}");
+  mc.moveDown();
+  print("${mc.y}");
+  mc.moveLeft();
+  print("${mc.ySpeed}");
+  mc.moveRight();
 
 
 }
