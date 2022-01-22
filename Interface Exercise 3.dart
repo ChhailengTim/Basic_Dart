87a34667c52d89911d50fd28bda99e041dc9c731
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-01-22 10:58:08 +0700

    Update Interface Exercise 3.dart

--- Interface Exercise 3.dart
+++ Interface Exercise 3.dart
@@ -20,10 +20,26 @@ class Animal implements Ifish,IBird,IMammal{
   }
 }
 
+class Person implements Ifish,IBird,IMammal{
+  void swim(){
+    print("People can swim on the water");
+  }
+  void fly(){
+    print("People can fly by airplane");
+  }
+  void milk(){
+    print("People give feed child by milk");
+  }
+}
 
 void main(){
   Animal a=new Animal();
   a.swim();
   a.fly();
   a.milk();
+  print("===================");
+  Person p=new Person();
+  p.swim();
+  p.fly();
+  p.milk();
 }
