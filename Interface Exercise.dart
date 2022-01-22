360d8720913b70972ad18c1e1da687391dbd9231
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-01-22 09:58:22 +0700

    Interface implement solve

--- Interface Exercise.dart
+++ Interface Exercise.dart
@@ -17,8 +17,24 @@ class Circle implements IGeometricObject{
   }
 }
 
+class Resizable{
+  int? resize(){}
+}
+
+class ResizableCircle extends Circle implements Resizable{
+  int? percent;
+  ResizableCircle(double radius,int percent):super(radius){
+    this.percent=percent;
+  }
+  int? resize(){
+    return percent;
+  }
+}
+
 void main(){
   Circle c=new Circle(2);
   print("Perimeter: ${c.getPerimeter()}");
   print("Area: ${c.getPerimeter()}");
+  ResizableCircle r=new ResizableCircle(2, 10);
+  print("Resize: ${r.resize()}");
 }
