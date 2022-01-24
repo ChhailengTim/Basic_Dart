552fc44991bcecf214ae512c8ea4ab8aa1baeda9
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-01-24 11:43:23 +0700

    Control statement

---
+++ Loop4.dart
@@ -0,0 +1,16 @@
+void main(){
+  int a=5;
+  int b=10;
+  int c=40;
+  if(a>b){
+    print(a);
+  }else if(a<b){
+    print(b);
+  }else if(a>0&&b>=0){
+    print(b);
+  }else if(a==0||c!=0){
+    print(b);
+  }else{
+    print("equal");
+  }
+}
