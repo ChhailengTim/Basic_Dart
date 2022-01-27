10c5332c7221c467529a754ddc394a57245771fd
CHHAILENGTIM <chhailengtim2018@gmail.com>
2022-01-27 11:49:11 +0700

    Super Keyword

---
+++ DemoSuperKeyword.dart
@@ -0,0 +1,12 @@
+class Vehicle{
+  //data member/field/variable
+  int speed=120;
+  //constructor
+  Vehicle(){
+    print("This is vehicle constructor");
+  }
+  //method
+  void Display(){
+    print("This is vehicle display");
+  }
+}
