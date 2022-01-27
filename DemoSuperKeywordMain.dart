7155ee21da40aae60996aa7ad5484b9dbbb47e51
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-28 00:21:22 +0700

    Super Key word how to use and usage in oop

---
+++ DemoSuperKeywordMain.dart
@@ -0,0 +1,12 @@
+import 'DemoSuperKeyword.dart';
+import 'DemoSuperKeywordCar.dart';
+
+void main(){
+  Vehicle v=new Vehicle();
+  v.Display();
+  print(v.speed);
+  print("========================");
+
+  Car c=new Car();
+  c.Show();
+}
