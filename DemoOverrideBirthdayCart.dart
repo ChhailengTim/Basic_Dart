efe0e5bca45465b23ac14747f86c8fa63ace84bf
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-28 00:19:20 +0700

    Overriding Method make me clarify

---
+++ DemoOverrideBirthdayCart.dart
@@ -0,0 +1,7 @@
+import 'DemoOverrideMethod.dart';
+class BirthdayCard extends Card{
+  //have all method and variable from parent class
+  String? MessageGreeting(){
+    print("Wish u on birthDAY");
+  }
+}
