865e8f2bc317eb1425150317604ac5930edeab6a
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-28 00:19:31 +0700

    Overriding Method make me clarify

---
+++ DemoOverrideHoldidayCard.dart
@@ -0,0 +1,11 @@
+import 'DemoOverrideMethod.dart';
+
+class HolidayCard extends Card{
+  String? MessageGreeting(){
+    print("Holiday is yr birthday");
+  }
+  /*  String? MessageGreeting(){
+    print("27/01/2022");
+  }
+   */
+}
