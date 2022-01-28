65152f08ebd09e886878d941e67eb6893437a283
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-29 00:13:22 +0700

    Make more clear with Encapsulation by oum saokosal

---
+++ Encapsulation7.dart
@@ -0,0 +1,13 @@
+import 'Encapsulation6.dart';
+
+class TestEx1{
+  TestEx1(){
+    Convertor c=new Convertor();
+    c.convert("Dara");
+    c.convert("12345");
+  }
+}
+
+void main(){
+  new TestEx1();
+}
