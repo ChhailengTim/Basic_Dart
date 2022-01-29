309501bc3227093b474a28bd43e762a3c59e82c9
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-30 00:09:27 +0700

    Make more clear with Encapsulation by oum saokosal

---
+++ Encapsulation5.dart
@@ -0,0 +1,15 @@
+class TestEX1{
+  void convert(String input){
+    String? enuser=input+"XOXO";
+    print(enuser);
+  }
+  TestEX1(){
+    convert("Dara");
+
+    convert("123");
+  }
+}
+
+void main(){
+  new TestEX1();
+}
