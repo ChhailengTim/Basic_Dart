b4a79f159c300b89f22fe399799d2b561f9b256d
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-30 00:09:21 +0700

    Make more clear with Encapsulation by oum saokosal

---
+++ Encapsulation4.dart
@@ -0,0 +1,15 @@
+class TestEX1{
+  TestEX1(){
+    String? user="Dara";
+    String? enuser=user+"XOXO";
+    print(enuser);
+
+    String? pass="123";
+    String? enpass=pass+"XOXO";
+    print(enpass);
+  }
+}
+
+void main(){
+  new TestEX1();
+}
