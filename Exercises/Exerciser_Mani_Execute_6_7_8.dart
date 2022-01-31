c86d8019dd9772242728199ccf3f64fb0dc8e7d7
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-01-31 22:51:31 +0700

    Update Exerciser_Mani_Execute_6_7_8.dart

--- Exercises/Exerciser_Mani_Execute_6_7_8.dart
+++ Exercises/Exerciser_Mani_Execute_6_7_8.dart
@@ -1,7 +1,14 @@
 import 'Exercises6.dart';
+import 'Exercises7.dart';
 void main(){
   Sweet1 s1=new Sweet1();
   s1.setSweet();
   s1.getSweet();
   print(s1.sn);
+  print("++++++++++++++++++++++++++++");
+  Sweet2 s2=new Sweet2();
+  s2.setSweet2();
+  s2.getSweet2();
+  print(s2.sn);
+  print("++++++++++++++++++++++++++++");
 }
