f75944355c23164300bc51f878798dd17327e9f6
ChhailengTim <80568234+ChhailengTim@users.noreply.github.com>
2022-02-11 23:07:22 +0700

    Update Encapsulation3.dart

--- Encapsulation3.dart
+++ Encapsulation3.dart
@@ -1,13 +1,14 @@
-class User{
+class User {
   String? email;
   String? name;
   String _password;
-  User(this.email,this.name,this._password);
+  User(this.email, this.name, this._password);
 
-  String getPassword(){
+  String setPassword() {
     return _password;
   }
-  void setPassword(String x){
-    _password=x;
+
+  void setPassword1(String x) {
+    _password = x;
   }
 }
